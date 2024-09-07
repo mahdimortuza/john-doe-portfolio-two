3be1745a830b3c9d862c1d8f4fb3baf24bddb4d6
mahdimortuza <mahdi.webx@gmail.com>
2024-09-07 13:50:31 +0600

    hover animation added

--- src/components/pageSections/HomePage/HeroSection.tsx
+++ src/components/pageSections/HomePage/HeroSection.tsx
@@ -1,18 +1,43 @@
+"use client";
 import Container from "@/components/Container";
 import ContainerInner from "@/components/ContainerInner";
-import Link from "next/link";
+import { useState } from "react";
+import { Link } from "react-scroll";
 
 const HeroSection = () => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <Container className=" ">
       <ContainerInner>
         <div className="flex justify-center items-center h-[100vh] ">
-          Hello
-          <Link href="/">
-            <button className="text-red hover:before:bg-redborder-red-500 relative h-[50px] w-40 overflow-hidden border border-red-500 bg-white px-3 text-red-500 shadow-2xl transition-all before:absolute before:bottom-0 before:right-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-red-500 before:transition-all before:duration-500 hover:text-white hover:shadow-red-500 hover:before:left-0 hover:before:w-full animate-bg-slide">
-              <span className="relative z-10">Swipe</span>
-            </button>
-          </Link>
+          <ul className="cursor-pointer">
+            <Link to="/">
+              <li className=" relative flex items-center justify-center mx-auto">
+                <span
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseOut={() => setIsHovered(false)}
+                  className={`${
+                    isHovered
+                      ? "text-theme-blue -translate-y-3 opacity-0"
+                      : "translate-y-0 opacity-100"
+                  } absolute duration-300 h-[30px] w-[50px] flex justify-center items-center`}
+                >
+                  Hello
+                </span>
+                <span
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseOut={() => setIsHovered(false)}
+                  className={`${
+                    isHovered
+                      ? " text-themeBlue  translate-y-0 opacity-100"
+                      : "translate-y-3 opacity-0"
+                  } absolute  duration-300 h-[30px] w-[50px] flex justify-center items-center`}
+                >
+                  Hello
+                </span>
+              </li>
+            </Link>
+          </ul>
         </div>
       </ContainerInner>
     </Container>
