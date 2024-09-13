a54e48bc695932f16a8289093f9bdb14c6cc21a3
mahdimortuza <mahdi.webx@gmail.com>
2024-09-13 10:02:07 +0600

    project data added

---
+++ src/db/projectsData.ts
@@ -0,0 +1,34 @@
+import assets from "@/assets";
+
+export const projectsData = [
+  {
+    _id: "1",
+    title: "apps design",
+    description: "john doe creative designer ",
+    image: assets.projects.project1,
+  },
+  {
+    _id: "2",
+    title: "apps design",
+    description: "john doe creative designer ",
+    image: assets.projects.project2,
+  },
+  {
+    _id: "3",
+    title: "landing page design",
+    description: "john doe creative designer ",
+    image: assets.projects.project3,
+  },
+  {
+    _id: "4",
+    title: "landing page design",
+    description: "john doe creative designer ",
+    image: assets.projects.project4,
+  },
+  {
+    _id: "5",
+    title: "landing page design",
+    description: "john doe creative designer ",
+    image: assets.projects.project5,
+  },
+];
